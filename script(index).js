ddd7c9de84642bb79fbb013a9cb09b2934ddde48
JanJakByte <janjakbyte@gmail.com>
2019-11-20 14:38:27 +0100

    V1

---
+++ script(index).js
@@ -0,0 +1,19 @@
+function over(){
+	document.getElementById("linkProjekte").style.width = "52%";
+	document.getElementById("linkDownloads").style.width = "48%";
+}
+
+function out(){
+	document.getElementById("linkProjekte").style.width = "50%";
+	document.getElementById("linkDownloads").style.width = "50%";
+}
+
+function over2(){
+	document.getElementById("linkDownloads").style.width = "52%";
+	document.getElementById("linkProjekte").style.width = "48%";
+}
+
+function out2(){
+	document.getElementById("linkDownloads").style.width = "50%";
+	document.getElementById("linkProjekte").style.width = "50%";
+}
